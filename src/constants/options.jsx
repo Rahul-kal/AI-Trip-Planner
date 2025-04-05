8ed4aaff8cc34996eb72ea2c104615e5141682b8
Rahul-kal <rahulkaliraman720@gmail.com>
2025-04-06 00:46:09 +0530

    1 hr compelete

---
+++ src/constants/options.jsx
@@ -0,0 +1,55 @@
+    export const SelectTravelsList = [
+        {
+        id: 1,
+        title: 'Just Me',
+        desc: 'A solo traveler in exploration',
+        icon: '✈️',
+        people: '1 Person',
+        },
+        {
+        id: 2,
+        title: 'A Couple',
+        desc: 'Two travelers exploring together',
+        icon: '🥂',
+        people: '2 People',
+        },
+        {
+        id: 3,
+        title: 'Family',
+        desc: 'A family adventure awaits',
+        icon: '🏡',
+        people: '3 to 5 People',
+        },
+        {
+        id: 4,
+        title: 'Friends',
+        desc: 'A bunch of thrill-seekers',
+        icon: '⛵',
+        people: '5 to 10 People',
+        },
+    ];
+    
+    export const SelectBudgetOptions = [
+        {
+        id: 1,
+        title: 'Cheap',
+        desc: 'Stay conscious of costs',
+        icon: '💵',
+        },
+        {
+        id: 2,
+        title: 'Moderate',
+        desc: 'Balanced budget with comfort',
+        icon: '💰',
+        },
+        {
+        id: 3,
+        title: 'Luxury',
+        desc: "Don't worry about the cost — it's all about experience",
+        icon: '💸'
+        },
+    ];
+    
+    // Prompt format for AI travel generation
+    export const AI_PROMPT = 'Generate Travel Plan for Location: {location}';
+    
