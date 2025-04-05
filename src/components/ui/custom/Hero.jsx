26c5f34ad2f02d61456e992c756756080bf381d5
Rahul-kal <rahulkaliraman720@gmail.com>
2025-04-05 18:07:26 +0530

    40 minutes

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Button } from '../button';
-import { useNavigate } from 'react-router-dom';
+    import React from 'react';
+    import { Button } from '../button';
+    import { useNavigate } from 'react-router-dom';
 
-function Hero() {
-  const navigate = useNavigate();
+    function Hero() {
+    const navigate = useNavigate();
 
-  return (
-    <div className="flex flex-col items-center mx-56 gap-9">
-      <h1 className="font-extrabold text-[60px] text-center mt-16">
-        <span className="text-[#f56551]">
-          Discover Your Next Adventure with AI
-        </span>
-        : Personalized Itineraries at Your Fingertips
-      </h1>
-      <p className="text-xl text-gray-500 text-center">
-        Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
-      </p>
-      <Button onClick={() => navigate('/create-trip')}>
-        Get Started, It's Free
-      </Button>
-    </div>
-  );
-}
+    return (
+        <div className="flex flex-col items-center mx-56 gap-9">
+        <h1 className="font-extrabold text-[60px] text-center mt-16">
+            <span className="text-[#f56551]">
+            Discover Your Next Adventure with AI
+            </span>
+            : Personalized Itineraries at Your Fingertips
+        </h1>
+        <p className="text-xl text-gray-500 text-center">
+            Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
+        </p>
+        <Button onClick={() => navigate('/create-trip')}>
+            Get Started, It's Free
+        </Button>
+        </div>
+    );
+    }
 
-export default Hero;
+    export default Hero;
