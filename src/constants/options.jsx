785a63ca4d89a439bebb7a7b628f31584f6d51c6
Rahul-kal <rahulkaliraman720@gmail.com>
2025-04-06 18:51:42 +0530

    1:38 min completed

--- src/constants/options.jsx
+++ src/constants/options.jsx
@@ -46,10 +46,11 @@
         id: 3,
         title: 'Luxury',
         desc: "Don't worry about the cost — it's all about experience",
-        icon: '💸'
+        icon: '💸',
         },
     ];
     
     // Prompt format for AI travel generation
-    export const AI_PROMPT = 'Generate Travel Plan for Location: {location}';
+    export const AI_PROMPT =
+        'Generate a travel plan for the location: {location}, for {totalDays} days for {traveler}, with a {budget} budget, give me Hotels options list with HotelName, Hotel address, Price ,hotel image url, geo coordinates, ticket Pricing, Time travel each of the location for {totalDays} days with each day plan with best time to visit in JSON format.';
     
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CreateTrip from './create-trip/index.jsx';
+import { Toaster } from 'sonner';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/create-trip" element={<CreateTrip />} />
-      </Routes>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <BrowserRouter>
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/create-trip" element={<CreateTrip />} />
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 );
