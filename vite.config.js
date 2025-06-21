ed53dde2a69c754ef4ec97194c11cf95c6be0c01
Rahul-kal <127105474+Rahul-kal@users.noreply.github.com>
2025-06-22 01:08:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
+
