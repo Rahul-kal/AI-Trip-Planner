b3f0e0646e9c94732bef4ec5cac9f802cc8b0e83
Rahul-kal <127105474+Rahul-kal@users.noreply.github.com>
2025-06-22 01:16:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
